8784f59f739acd9f534b96e188e69421d3dcfdd9
Miles Budden <miles@budden.net>
2015-12-01 21:35:41 +0000

    Made the views output the scraped data
    Also uploaded an example photo of the output

--- newsgraph/news/views.py
+++ newsgraph/news/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import urllib.request
+from .models import Story
+import feedparser
 
-from .models import Node
+def stories():
+	# Connect to the BBC RSS feed
+	news = feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml?edition=uk')
+	# For every story
+	for story in news.entries:
+		# Return the link to that story
+		yield story['link']
 
 def index(request):
-	nodes = Node.objects.all()
-	output = ', '.join([i.name for i in nodes])
+	for story in stories():
+		s = Story(source=story)
+		s.save()
+		# print("story:", story)
+		# with urllib.request.urlopen(story) as response:
+		# 	html = response.read()
+		# 	soup = BeautifulSoup(html, "html.parser")
+		# 	try:
+		# 		print(soup.find("div", {"class": "story-body"}).get_text())
+		# 	except:
+		# 		print("No content found")
+	stories_add = Story.objects.all()
+	output = '<br>======================================================================<br>'.join([i.source for i in stories_add])
 	print(output)
 	return HttpResponse(output)
