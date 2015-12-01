f39adaba2f974bf366115bc62358c2faf912dbe8
Miles Budden <miles@budden.net>
2015-12-01 19:00:16 +0000

    It is now extracting data from news articles

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from BBC.getStories import stories
+from bs4 import BeautifulSoup
+import urllib.request
 
 for story in stories():
-	print(story)
+	print("story:", story)
+	with urllib.request.urlopen(story) as response:
+		html = response.read()
+		soup = BeautifulSoup(html, "html.parser")
+		try:
+			print(soup.find("div", {"class": "story-body"}).get_text())
+		except:
+			print("No content found")
+		
