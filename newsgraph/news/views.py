869088d5f19612393410c860dc28f140c3258ca5
Miles Budden <miles@budden.net>
2015-12-06 20:00:46 +0000

    Optimised views

--- newsgraph/news/views.py
+++ newsgraph/news/views.py
@@ -7,6 +7,7 @@ from .models import Story, Node, Edge
 import feedparser
 import nltk
 
+# Returns a list of stories currently on the front page of the BBC website
 def stories():
 	# Connect to the BBC RSS feed
 	news = feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml?edition=uk')
@@ -15,12 +16,14 @@ def stories():
 		# Return the link to that story
 		yield story['link']
 
+# Tokenizes the input
 def prepareForNLP(text):
 	sentences = nltk.sent_tokenize(text)
 	sentences = [nltk.word_tokenize(sent) for sent in sentences]
 	sentences = [nltk.pos_tag(sent) for sent in sentences]
 	return sentences
 
+# Yields the keywords (NNPs) from the input as tuples
 def keywords(text):
 	sentences = prepareForNLP(text)
 	for sentence in sentences:
@@ -28,6 +31,7 @@ def keywords(text):
 			if word[1] == "NNP":
 				yield word[0]
 
+# Returns the keywords (NNPs) from the input as a list of tuples
 def keywordsList(text):
 	output = []
 	sentences = prepareForNLP(text)
@@ -37,9 +41,13 @@ def keywordsList(text):
 				output.append(word[0])
 	return output
 
+# Updates the DB to a more recent version of the news
 def updateDB():
+	# For each story currently on the BBC RSS feed
 	for story in stories():
+		# More than 0 if the story is in the DB
 		matches = Story.objects.filter(source=story)
+		# If the story isn't in the database
 		if len(matches) == 0:
 			print("Adding story:", story)
 			content = ""
@@ -54,6 +62,7 @@ def updateDB():
 					print("No content found")
 			s = Story(source=story, content=content)
 			s.save()
+			content = content.replace(".", ". ")
 			for kw in keywords(content):
 				DBalready = []
 				if kw not in DBalready:
