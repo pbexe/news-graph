e0f2496dbf7c900d7e10d61d45b27b40782fbbab
Miles Budden <miles@budden.net>
2016-02-03 21:57:33 +0000

    Improved table styling

--- mysite/relationships/views.py
+++ mysite/relationships/views.py
@@ -5,13 +5,17 @@ from .models import Story, Node, Edge
 def index(request):
 	# Get a list of all nodes
 	edge_add = Edge.objects.all()
-	output = ""
+	output = """<style>table, th, td {
+    border: 1px solid black;border-collapse: collapse;
+	}</style>"""
+	output += "<table>"
 	# For each edge
 	for edge in edge_add:
+		output += '<tr>'
 		# Add that edge to the output
-		output += edge.origin.name
-		output += " => "
-		output += edge.destination.name
-		output += "<br />"
+		output += '<td>' + edge.origin.name + '</td>'
+		output += '<td>' + edge.destination.name + '</td>'
+		output += "</tr>"
+	output += '</table>'
 	# Return the output to the client
 	return HttpResponse(output)
