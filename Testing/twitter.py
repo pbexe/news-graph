37400908cfcb787d99de499bbe90abe56dc85e73
Miles Budden <miles@budden.net>
2016-07-04 14:29:57 +0100

    Venv

---
+++ Testing/twitter.py
@@ -0,0 +1,87 @@
+from nltk.corpus import twitter_samples
+import nltk
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+print twitter_samples.fileids()
+
+posTags = {}
+negTags = {}
+
+tokenized = twitter_samples.tokenized('positive_tweets.json')
+for toks in tokenized:
+	toks = nltk.pos_tag(toks)
+	for word in toks:
+		if word[1] in posTags:
+			posTags[word[1]] += 1
+		else:
+			posTags[word[1]] = 1
+
+tokenized = twitter_samples.tokenized('negative_tweets.json')
+for toks in tokenized:
+	toks = nltk.pos_tag(toks)
+	for word in toks:
+		if word[1] in negTags:
+			negTags[word[1]] += 1
+		else:
+			negTags[word[1]] = 1
+
+posX = []
+posY = []
+
+for key in posTags:
+	posX.append(key)
+	posY.append(posTags[key])
+
+negX = []
+negY = []
+
+for key in negTags:
+	negX.append(key)
+	negY.append(negTags[key])
+
+allTags = {}
+for tag in posTags:
+	if tag in negTags:
+		allTags[tag] = posTags[tag] - negTags[tag]
+	else:
+		allTags[tag] = posTags[tag]
+for tag in negTags:
+	if tag not in allTags:
+		allTags[tag] = 0 - negTags[tag]
+
+X = []
+Y = []
+
+for key in allTags:
+	X.append(key)
+	Y.append(allTags[key])
+
+YX = zip(Y,X)
+YX.sort()
+
+Ynew = [Y for Y,X in YX]
+Xnew = [X for Y,X in YX]
+print Xnew
+print Ynew
+
+trace1 = go.Bar(
+    x=Xnew,
+    y=Ynew,
+    name='Tweets'
+)
+# trace2 = go.Bar(
+#     x=negX,
+#     y=negY,
+#     name='Negative Tweets'
+# )
+
+data = [trace1]
+layout = go.Layout(
+    barmode='group'
+)
+
+fig = go.Figure(data=data, layout=layout)
+py.plot(fig, filename='grouped-bar')
+
+    
--- src/updateDB.py
+++ src/updateDB.py
@@ -1,4 +1,3 @@
-
 # Import the library needed to interact with the operating system
 import os
 
@@ -30,80 +29,80 @@ import nltk
 import sys
 
 def stories():
-	news = feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml?edition=uk')
-	for text in news.entries:
-		yield (text.link, text.description)
+    news = feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml?edition=uk')
+    for text in news.entries:
+        yield (text.link, text.description)
 
 # Tokenizes the input so it can be analysed
 def prepareForNLP(text):
-	# Split up the input into sentences
-	sentences = nltk.sent_tokenize(text)
-	# Split up the sentences into words
-	sentences = [nltk.word_tokenize(sent) for sent in sentences]
-	# Tokenize the 
-	sentences = [nltk.pos_tag(sent) for sent in sentences]
-	# Return the split and tokenized sentences
-	return sentences
+    # Split up the input into sentences
+    sentences = nltk.sent_tokenize(text)
+    # Split up the sentences into words
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    # Tokenize the 
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    # Return the split and tokenized sentences
+    return sentences
 
 def chunk(sentence):
-	# Chunking pattern
-	chunkToExtract = """
-	NP: {<NNP>*}"""
-	# Create the new parser
-	parser = nltk.RegexpParser(chunkToExtract)
-	# Parse the text
-	result = parser.parse(sentence)
-	# Yield the proper noun phrases
-	for subtree in result.subtrees():
-		if subtree.label() == 'NP':
-			t = subtree
-			t = ' '.join(word for word, pos in t.leaves())
-			yield t
+    # Chunking pattern
+    chunkToExtract = """
+    NP: {<NNP>*}"""
+    # Create the new parser
+    parser = nltk.RegexpParser(chunkToExtract)
+    # Parse the text
+    result = parser.parse(sentence)
+    # Yield the proper noun phrases
+    for subtree in result.subtrees():
+        if subtree.label() == 'NP':
+            t = subtree
+            t = ' '.join(word for word, pos in t.leaves())
+            yield t
 
 # Yields the keywords (NNPs) from the input as tuples
 def keywords(text):
-	sentences = prepareForNLP(text)
-	for sentence in sentences:
-		for kw in chunk(sentence):
-			yield kw
+    sentences = prepareForNLP(text)
+    for sentence in sentences:
+        for kw in chunk(sentence):
+            yield kw
 
 def makeEdges(nodes, story):
-	while True:
-		if len(nodes) > 0:
-			node = nodes[0]
-			nodes.remove(node)
-			for i in nodes:
-				edge = Edge(source=story,origin=node,destination=i)
-				edge.save()
-		else:
-			break
+    while True:
+        if len(nodes) > 0:
+            node = nodes[0]
+            nodes.remove(node)
+            for i in nodes:
+                edge = Edge(source=story,origin=node,destination=i)
+                edge.save()
+        else:
+            break
 
 def addStory(story):
-	print("Adding story:", story[0])
-	s = Story(source=story[0], content=story[1])
-	s.save()
-	nodes = []
-	for kw in keywords(story[1]):
-		if len(Node.objects.filter(name=kw)) < 1:
-			node = Node(name=kw,date=timezone.now(),collectedFrom=s)
-			node.save()
-		else:
-			node = Node.objects.filter(name=kw)[0]
-			node.date = timezone.now()
-		nodes.append(node)
-	makeEdges(nodes, story[0])
+    print("Adding story:", story[0])
+    s = Story(source=story[0], content=story[1])
+    s.save()
+    nodes = []
+    for kw in keywords(story[1]):
+        if len(Node.objects.filter(name=kw)) < 1:
+            node = Node(name=kw,date=timezone.now(),collectedFrom=s)
+            node.save()
+        else:
+            node = Node.objects.filter(name=kw)[0]
+            node.date = timezone.now()
+        nodes.append(node)
+    makeEdges(nodes, story[0])
 
 # Updates the DB to a more recent version of the news
 def updateDB():
-	# For each story currently on the BBC RSS feed
-	for story in stories():
-		# More than 0 if the story is in the DB
-		matches = Story.objects.filter(source=story[0])
-		# If the story isn't in the database
-		if len(matches) == 0:
-			addStory(story)
-		else: pass
+    # For each story currently on the BBC RSS feed
+    for story in stories():
+        # More than 0 if the story is in the DB
+        matches = Story.objects.filter(source=story[0])
+        # If the story isn't in the database
+        if len(matches) == 0:
+            addStory(story)
+        else: pass
 
 if __name__ == "__main__":
-	print("Updating DB")
-	updateDB()
+    print("Updating DB")
+    updateDB()
