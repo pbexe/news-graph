c944f98101a29f9b20c7f1931167620aadf59439
Miles Budden <miles@budden.net>
2017-01-31 21:00:08 +0000

    Added naive bayes classifier code
    It actually works!?
    
    Former-commit-id: 1aa4d3b9d8518dc255b6875199ab1aab18df93f1

--- Testing/NeuralNet/prepare.py
+++ Testing/NeuralNet/prepare.py
@@ -6,6 +6,7 @@ from collections import Counter
 from nltk.stem import WordNetLemmatizer
 import numpy
 import random
+import pickle
 
 print(twitter_samples.fileids())
 # Init the lemmatizer
@@ -13,7 +14,7 @@ lemmatizer = WordNetLemmatizer()
 
 
 # Build the lexicon
-def lexicon():
+def lexicon_old():
     # Stop words to ignore
     stop_words = set(stopwords.words('english'))
     # Load the tweets
@@ -28,28 +29,52 @@ def lexicon():
     for word in lexicon_frequency:
         if (len(lexicon) * 0.8) > lexicon_frequency[word]:
             lexicon_filtered.append(word)
+    with open('lex.data','wb') as fp:
+        pickle.dump(lexicon_filtered, fp)
     return lexicon_filtered
 
+# Build the lexicon
+def lexicon():
+    # Stop words to ignore
+    stop_words = set(stopwords.words('english'))
+    with open('pos.txt', 'r') as fp:
+        pos = fp.read()
+    with open('neg.txt', 'r') as fp:
+        neg = fp.read()
+    words = pos + neg
+    # Before and after filtering arrays
+    lexicon = []
+    lexicon_filtered = []
+    # Create list of words from data set
+    [lexicon.append(lemmatizer.lemmatize(word)) for word in words.split(' ') if word not in stop_words]
+    # Make each item unique
+    lexicon_frequency = Counter(lexicon)
+    for word in lexicon_frequency:
+        if (len(lexicon) * 0.8) > lexicon_frequency[word]:
+            lexicon_filtered.append(word)
+    with open('lex.data','wb') as fp:
+        pickle.dump(lexicon_filtered, fp)
+    return lexicon_filtered
 
 def parse_sample(sample, sentiment, lexicon):
     oppinion_sentence_with_sentiment = []
-    tweets = twitter_samples.strings(sample)
-    for line in tweets:
-        line = word_tokenize(line.lower())
-        line = [lemmatizer.lemmatize(word) for word in line]
-        lexicon_mask = numpy.zeros(len(lexicon))
-        for word in line:
-            if word.lower() in lexicon:
-                index_value = lexicon.index(word.lower())
-                lexicon_mask[index_value] += 1
-        oppinion_sentence_with_sentiment.append([list(lexicon_mask), sentiment])
+    with open(sample, 'r') as fp:
+        for line in fp.readlines():
+            line = word_tokenize(line.lower())
+            line = [lemmatizer.lemmatize(word) for word in line]
+            lexicon_mask = numpy.zeros(len(lexicon))
+            for word in line:
+                if word.lower() in lexicon:
+                    index_value = lexicon.index(word.lower())
+                    lexicon_mask[index_value] += 1
+            oppinion_sentence_with_sentiment.append([list(lexicon_mask), sentiment])
     return oppinion_sentence_with_sentiment
 
 
 def create_training_data(lexicon):
     data = []
-    data += parse_sample('negative_tweets.json', [1, 0], lexicon)
-    data += parse_sample('positive_tweets.json', [0, 1], lexicon)
+    data += parse_sample('pos.txt', [1, 0], lexicon)
+    data += parse_sample('neg.txt', [0, 1], lexicon)
     random.shuffle(data)
     test_data = int(len(data)*0.1)
     x_training_data = []
@@ -65,7 +90,3 @@ def create_training_data(lexicon):
     for item in data[-test_data:]:
         y_test_data.append(item[1])
     return x_training_data, y_training_data, x_test_data, y_test_data
-
-
-if __name__ == "__main__":
-    parse_sample('pos.txt', [1, 0], lexicon())
