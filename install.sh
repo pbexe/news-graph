68ce535208941bbdf1a2f799d7dd22f4a19b6b45
Miles <miles@budden.net>
2017-03-08 09:42:34 +0000

    Fixed installer

--- install.sh
+++ install.sh
@@ -8,6 +8,15 @@ echo '===================================================='
 sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data stopwords
 cd src/
 echo '===================================================='
+echo 'Check for DB changes'
+echo '===================================================='
+python3 manage.py makemigrations
+echo '===================================================='
+echo 'Build DB'
+echo '===================================================='
+python3 manage.py migrate
+echo '===================================================='
 echo 'Updating DB'
 echo '===================================================='
 python3 updateDB.py
+read -n 1 -s -p "Press any key to continue"
