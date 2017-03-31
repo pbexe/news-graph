5f3745cf9af04a935ad6419443ea38e576609408
Miles Budden <pbexe@users.noreply.github.com>
2017-03-31 19:05:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-echo '===================================================='
-echo 'Installing requirements'
-echo '===================================================='
-sudo pip3 install -r requirements.txt
+# echo '===================================================='
+# echo 'Installing requirements'
+# echo '===================================================='
+# sudo pip3 install -r requirements.txt
 echo '===================================================='
 echo 'Downloading required NLTK libs'
 echo '===================================================='
-sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data stopwords book
+python -m nltk.downloader -d /usr/local/share/nltk_data stopwords book
 cd src/
 echo '===================================================='
 echo 'Check for DB changes'
 echo '===================================================='
-python3 manage.py makemigrations
+python manage.py makemigrations
 echo '===================================================='
 echo 'Build DB'
 echo '===================================================='
-python3 manage.py migrate
+python manage.py migrate
 echo '===================================================='
 echo 'Updating DB'
 echo '===================================================='
-python3 updateDB.py
+python updateDB.py
